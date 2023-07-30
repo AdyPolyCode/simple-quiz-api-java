75ab291735d3c07697277a0295b84347d859da73
adypolycode <polcsek.dm90@gmail.com>
2023-07-30 11:57:57 +0200

    init question service

--- src/main/java/com/poly/quizapi/services/QuestionService.java
+++ src/main/java/com/poly/quizapi/services/QuestionService.java
@@ -1,4 +1,16 @@
 package com.poly.quizapi.services;
 
+import com.poly.quizapi.repositories.QuestionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class QuestionService {
+
+    QuestionRepository repository;
+
+    @Autowired
+    public QuestionService(QuestionRepository repository) {
+        this.repository = repository;
+    }
 }
