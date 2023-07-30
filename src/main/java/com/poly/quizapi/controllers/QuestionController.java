4463cf904d17fa8395e4ec22f322119c9cc8e1e3
adypolycode <polcsek.dm90@gmail.com>
2023-07-30 13:05:52 +0200

    add init CRUD methods

--- src/main/java/com/poly/quizapi/controllers/QuestionController.java
+++ src/main/java/com/poly/quizapi/controllers/QuestionController.java
@@ -1,10 +1,11 @@
 package com.poly.quizapi.controllers;
 
+import com.poly.quizapi.models.Question;
 import com.poly.quizapi.services.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("questions")
@@ -20,5 +21,25 @@ public class QuestionController {
     @GetMapping("test")
     public String test() {
         return "Hello from " + QuestionController.class.getName();
+    }
+
+    @PostMapping
+    public ResponseEntity<Question> create(@RequestBody final Question question) {
+        return service.create();
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Question> getById(@PathVariable Integer id) {
+        return service.getById();
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Question> updateById(@PathVariable Integer id) {
+        return service.updateById();
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Question> deleteById(@PathVariable Integer id) {
+        return service.deleteById();
     }
 }
--- src/main/java/com/poly/quizapi/services/QuestionService.java
+++ src/main/java/com/poly/quizapi/services/QuestionService.java
@@ -1,7 +1,10 @@
 package com.poly.quizapi.services;
 
+import com.poly.quizapi.models.Question;
 import com.poly.quizapi.repositories.QuestionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,5 +15,37 @@ public class QuestionService {
     @Autowired
     public QuestionService(QuestionRepository repository) {
         this.repository = repository;
+    }
+
+    public ResponseEntity<Question> create() {
+        Question question = null;
+        return new ResponseEntity<>(
+                question,
+                HttpStatus.CREATED
+        );
+    }
+
+    public ResponseEntity<Question> getById() {
+        Question question = null;
+        return new ResponseEntity<>(
+                question,
+                HttpStatus.OK
+        );
+    }
+
+    public ResponseEntity<Question> updateById() {
+        Question question = null;
+        return new ResponseEntity<>(
+                question,
+                HttpStatus.OK
+        );
+    }
+
+    public ResponseEntity<Question> deleteById() {
+        Question question = null;
+        return new ResponseEntity<>(
+                question,
+                HttpStatus.NO_CONTENT
+        );
     }
 }
