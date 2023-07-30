773f2f1e4c77191ae3b9d3e2c232de855c1b87c4
adypolycode <polcsek.dm90@gmail.com>
2023-07-30 11:57:45 +0200

    init question controller

--- src/main/java/com/poly/quizapi/controllers/QuestionController.java
+++ src/main/java/com/poly/quizapi/controllers/QuestionController.java
@@ -1,4 +1,24 @@
 package com.poly.quizapi.controllers;
 
+import com.poly.quizapi.services.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("questions")
 public class QuestionController {
+
+    QuestionService service;
+
+    @Autowired
+    public QuestionController(QuestionService service) {
+        this.service = service;
+    }
+
+    @GetMapping("test")
+    public String test() {
+        return "Hello from " + QuestionController.class.getName();
+    }
 }
