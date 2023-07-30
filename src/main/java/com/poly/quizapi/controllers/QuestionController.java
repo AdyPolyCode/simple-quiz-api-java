e1959cbc56cd06094b57b168e407dcc3acdbfe61
adypolycode <polcsek.dm90@gmail.com>
2023-07-30 15:04:38 +0200

    add get all method with and without request query param

--- src/main/java/com/poly/quizapi/controllers/QuestionController.java
+++ src/main/java/com/poly/quizapi/controllers/QuestionController.java
@@ -3,9 +3,10 @@ package com.poly.quizapi.controllers;
 import com.poly.quizapi.models.Question;
 import com.poly.quizapi.services.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("questions")
@@ -20,26 +21,36 @@ public class QuestionController {
 
     @GetMapping("test")
     public String test() {
-        return "Hello from " + QuestionController.class.getName();
+        return "Hello from " + QuestionController.class.getSimpleName();
     }
 
     @PostMapping
     public ResponseEntity<Question> create(@RequestBody final Question question) {
-        return service.create();
+        return service.create(question);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Question>> getAll() {
+        return service.getAll();
+    }
+
+    @GetMapping("random")
+    public ResponseEntity<List<Question>> getSomeByCategory(@RequestParam(name = "category") String cat, @RequestParam(name = "numberOfCategories") Integer numOfCat) {
+        return service.getSomeByCategory(cat, numOfCat);
     }
 
     @GetMapping("{id}")
     public ResponseEntity<Question> getById(@PathVariable Integer id) {
-        return service.getById();
+        return service.getById(id);
     }
 
     @PutMapping("{id}")
-    public ResponseEntity<Question> updateById(@PathVariable Integer id) {
-        return service.updateById();
+    public ResponseEntity<Question> updateById(@RequestBody final Question question, @PathVariable Integer id) {
+        return service.updateById(id, question);
     }
 
     @DeleteMapping("{id}")
     public ResponseEntity<Question> deleteById(@PathVariable Integer id) {
-        return service.deleteById();
+        return service.deleteById(id);
     }
 }
