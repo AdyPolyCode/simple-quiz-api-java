12561959016be365ae067d5165a3036fb57ca542
adypolycode <polcsek.dm90@gmail.com>
2023-07-30 11:57:52 +0200

    init question repository

--- src/main/java/com/poly/quizapi/repositories/QuestionRepository.java
+++ src/main/java/com/poly/quizapi/repositories/QuestionRepository.java
@@ -1,4 +1,9 @@
 package com.poly.quizapi.repositories;
 
-public interface QuestionRepository {
+import com.poly.quizapi.models.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface QuestionRepository extends JpaRepository<Question, Integer> {
 }
