ba01be9ec283fe003b9268bb1b2a1f90c264f537
adypolycode <polcsek.dm90@gmail.com>
2023-07-30 15:05:02 +0200

    add custom get to fetch by category

--- src/main/java/com/poly/quizapi/repositories/QuestionRepository.java
+++ src/main/java/com/poly/quizapi/repositories/QuestionRepository.java
@@ -3,7 +3,13 @@ package com.poly.quizapi.repositories;
 import com.poly.quizapi.models.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
+
+    @Query("SELECT q FROM Question q WHERE q.category = :cat ORDER BY RANDOM() LIMIT :numberOfQuestion")
+    List<Question> findSomeByCategory(String cat, int numberOfQuestion);
 }
